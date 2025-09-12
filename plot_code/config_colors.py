7447414bbf9216c605ad2692963068cf7611d5ab
Alana Darcher <alana.darcher@gmail.com>
2025-09-12 10:59:37 +0200

    replotting with region changes and new isi calculation

--- plot_code/config_colors.py
+++ plot_code/config_colors.py
@@ -14,14 +14,11 @@ colors = [
 ]
 
 color_by_region = {
-    "PIC": '#fdbf6f',
     "A": '#E53E24', 
     "H": '#54aead', 
-    "EC": '#FFFF33', 
+    "EC": '#F7F708', 
     "PHC": '#bfe5a0', 
-    "FF": '#0A2463',
-    "LG": '#3B5738',
-    "PRC": '#6E1A0D',
+    "Other": '#fdbf6f',
 }
 
 cmap = sns.color_palette("Spectral", as_cmap=True)

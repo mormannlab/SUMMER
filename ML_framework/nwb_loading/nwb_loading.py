3ef216f5867c6482c9c911be1de8df32b12e822d
Franziska Gerken <fgerken@ipp1-1834.ipp1a1.colossus.nvidia.com>
2026-05-18 15:58:05 +0000

    clean up & fix binning

--- ML_framework/nwb_loading/nwb_loading.py
+++ ML_framework/nwb_loading/nwb_loading.py
@@ -19,17 +19,17 @@ def load_nwb(patient, data_dir=None):
         tuple: A tuple containing the loaded NWB file and the path to the NWB file.
     """
     base = Path(data_dir) if data_dir is not None else Path(DEFAULT_DATA_DIR)
+    paths = list(Path(base).glob(f"sub{int(patient)}*.nwb"))
 
-    path = Path(base).glob(f"sub-{int(patient)}*.nwb")
-
-    if len(list(path)) == 0:
+    if len(paths) == 0:
         path = Path(data_dir, f"sub-{int(patient)}")
         path = path.glob(f"sub-{int(patient)}*.nwb")
-    
-    nwb_file = list(path)[0]
-    
-    if not nwb_file.exists():
-        raise FileNotFoundError(f"NWB file for patient {patient} not found at {nwb_file}")
+
+    if len(paths) == 0:
+        raise FileNotFoundError(
+            f"NWB file for patient {patient} not found in {base}"
+        )
+    nwb_file = paths[0]
     
     io = NWBHDF5IO(nwb_file, mode="r")
     nwb_data = io.read()
